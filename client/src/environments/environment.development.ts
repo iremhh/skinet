92e07980df1de59fabeaaac626a0d6a32e3914d5
İrem Hafızoğlu <iremhafizoglu@gmail.com>
2025-08-25 10:54:30 +0300

    End of section 18

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
+    hubUrl: 'https://localhost:5001/hub/notifications',
     stripePublicKey: 'pk_test_51RvyIxGy62gyEKFTsZONqGAX8ydUo04Dc9oT4hHFIoY2PxjjasAig0nybIWe7VIcgvYCDkD1yF6QjrKLVPOhfNno00l0jOSMTD'
 };
